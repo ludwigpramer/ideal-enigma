9a2b1889ad073032f02a95a4ad3ba9c1e1e44e85
Coolertyo4 <lm.pramer@gmail.com>
2021-10-10 21:47:44 +0200

    mM

---
+++ src/SecondChapter/Enums/Enum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+enum mois {
+     janvier = 1, fevrier, mars, avril, mai, juin, juillet, aout, septembre, octobre, novembre, decembre
+};
+int main()
+{
+     printf("%d", aout);
+}

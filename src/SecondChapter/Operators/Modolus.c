a58d86c0b113e8baaf14557fe4ce64189bb8d882
Coolertyo4 <lm.pramer@gmail.com>
2021-10-11 15:35:34 +0200

    h

--- src/SecondChapter/Operators/Modolus.c
+++ src/SecondChapter/Operators/Modolus.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 
-// #define true 1
-// #define false 0
-
-#define startYear 2020 
+#define startYear 0 
 #define endYear 3000
+
+
+int numberOfLeapYears = 0;
+int currentIndex = 0;
+int result[endYear - startYear + 1];
+
+void isYear(int year)
+{
+     
+     result[currentIndex] = year;
+     ++currentIndex; 
+     ++numberOfLeapYears;
+     printf("%d is a leap year!\n", year);
+}
 
 int main()
 {
      
      for(int i = endYear; i >= startYear; --i)
-          i % 4 == 0 || i % 100 == 0 || i % 400 == 0 ? printf("%d is a leap year!\n", i) : printf("%d is not a leap year!\n", i);
+          i % 4 == 0 || i % 100 == 0 || i % 400 == 0 ? isYear(i) : printf("%d is not a leap year!\n", i);
+     printf("%d leap years found\n", numberOfLeapYears);
+    
+     for(int j = 0; j <= sizeof(result)/sizeof(int)-1; ++j)
+          if(result[j] != 0)
+               printf("%d ", result[j]);
+     printf("\n");
+     
+
 }
+
+
 
